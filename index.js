5a7470866c8268847316e4deaf76e2e630193e7b
anushkaMohan <mohananushka861@gmail.com>
2023-02-19 11:33:04 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express=require('express');
+const port=8000;
+const app=express();
+
+
+
+app.listen(port,function(err){
+      if(err){
+        console.log(`Error occured : ${err}`);
+        return;
+      }
+      console.log(`Server is up and running on port : ${port}`);
+})
+
