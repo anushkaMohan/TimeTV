6e04be5127ec9bfd5db1772d2bb366013a102d86
anushkaMohan <mohananushka861@gmail.com>
2023-03-10 16:05:31 +0530

    authentication changes

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,7 +1,26 @@
 const User=require('../models/user');
 
 module.exports.profile=function(req,res){
-    res.end('<h1>User Profile</h1>');
+        if(req.cookies.user_id){
+            User.findById(req.cookies.user_id,function(err,user){
+                if(user){
+                    return res.render('user_profile',{
+                        title : 'User Profile',
+                        user: user
+                    })
+                }
+                else{
+                    return res.redirect('users/sign-in');
+                }
+            })
+        }
+
+
+
+
+
+
+    
 }
 
 module.exports.signUp=function(req,res){
@@ -44,5 +63,24 @@ module.exports.create=function(req,res){
 
 //get signIn data
 module.exports.createSession=function(req,res){
-
+    //find user
+    User.findOne({email:req.body.email},function(err,user){
+        if(err){
+            console.log("Error in finding user!");
+            return;
+        }
+        if(!user){
+           return res.redirect('/users/sign-in');
+        }
+        else{
+            if(user.password !=req.body.password){
+                return res.redirect('/users/sign-in')
+            }
+            else{
+                    //handle session creation
+                    res.cookie('user_id',user.id);
+                    return res.redirect('/users/profile');
+            }
+        }
+    })
 }
--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,12 @@ const express=require('express');
 const router=express.Router();
 
 const usersController=require('../controllers/usersController');
+router.get('/profile',usersController.profile);
 router.get('/sign-up',usersController.signUp);
 router.get('/sign-in',usersController.signIn);
-router.post('/create',usersController.create)
+
+router.post('/create',usersController.create);
+router.post('/create-session',usersController.createSession)
 
 module.exports=router;
 
