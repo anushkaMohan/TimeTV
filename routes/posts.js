8457b0b36bc3477d7e3d791f9b5d8b7aabd4a127
anushkaMohan <mohananushka861@gmail.com>
2023-03-12 12:50:47 +0530

    added post router and controller

---
+++ routes/posts.js
@@ -0,0 +1,8 @@
+const express=require('express');
+const router=express.Router();
+
+const postsController=require('../controllers/postsController');
+
+router.post('/create',postsController.create);
+
+module.exports=router;
