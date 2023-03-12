6c1ff9217e07a197470514f78e924ee656039614
anushkaMohan <mohananushka861@gmail.com>
2023-03-12 13:10:25 +0530

    Added posts to home page

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,9 +1,20 @@
+const Post=require('../models/post');
 //File used to handle various actions
 module.exports.home= function(req,res){
   //  return res.end('<h1>Express is bopping popping!');
-   console.log(req.cookies)
-   res.cookie('user_1',23);
-   return res.render('home',{
-     title:'Home'
-   });
+  //  console.log(req.cookies)
+  //  res.cookie('user_1',23);
+   
+  //  Post.find({},function(err,posts){
+  //   return res.render('home',{
+  //     title:'TimeTV | Home',
+  //     posts : posts
+  //   });
+  //  })
+   Post.find({}).populate('user').exec(function(err,posts){
+          return res.render('home',{
+            title:'TimeTV | Home',
+            posts : posts
+          });
+   })
 }
